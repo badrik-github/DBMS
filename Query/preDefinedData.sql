bdd8a650f0127435e6d559ea0d710039166bbf06
PATEL BADRIK MAHESH <patelbadrik@gmail.com>
2022-12-05 06:41:35 +0530

    added predefined data and solvec bugs into table

---
+++ Query/preDefinedData.sql
@@ -0,0 +1,78 @@
+INSERT INTO
+    area (pin_code, state, district)
+VALUES
+    (
+        380058,
+        'Gujarat',
+        'Ahmedabad'
+    ),
+    (
+        382160,
+        'Gujarat',
+        'Ahmedabad'
+    );
+
+INSERT INTO
+    branch(
+        pin_code,
+        IFSC_code,
+        branch_name,
+        address
+    )
+VALUES
+    (
+        380058,
+        'BOBBOPAL67',
+        'BankOfBarodaBopal',
+        'Plot No. 5, Vruj Vihar Society, Shri Hari House, Ahmedabad, Gujarat 380058'
+    );
+
+INSERT INTO
+    employee(
+        IFSC_code,
+        name,
+        address,
+        pan_number,
+        adhare_number,
+        salary,
+        bob,
+        date_of_joining,
+        gender
+    )
+VALUES
+    (
+        'BOBBOPAL67',
+        'devdutt',
+        '73 dev darshan tenament, Near JB park, Opp sterling city, Bopal Ahmedabad',
+        'BNZPF2501F',
+        '000032418907',
+        50000,
+        '1995-08-13',
+        '2015-01-01',
+        'male'
+    );
+
+INSERT INTO
+    employee(
+        IFSC_code,
+        name,
+        address,
+        pan_number,
+        adhare_number,
+        salary,
+        dob,
+        date_of_joining,
+        gender
+    )
+VALUES
+    (
+        'BOBBOPAL67',
+        'devanshu',
+        '89 shyamal villa, Opp garden pelace, sindhu bhavan road,Ahmedabad',
+        'KCRI345OZ9',
+        '123459903323',
+        90000,
+        '1990-08-13',
+        '2011-12-01',
+        'male'
+    );
--- Schema/tables.sql
+++ Schema/tables.sql
@@ -8,40 +8,34 @@ CREATE TABLE IF NOT EXISTS branch(
     pin_code integer NOT NULL,
     IFSC_code varchar(50) PRIMARY KEY,
     branch_name varchar(50) NOT NULL,
-    address varchar(50) NOT NULL,
-
-    CONSTRAINT branch_in_area
-    FOREIGN KEY(pin_code) 
-    REFERENCES area(pin_code)
+    address varchar(400) NOT NULL,
+    CONSTRAINT branch_in_area FOREIGN KEY(pin_code) REFERENCES area(pin_code)
 );
 
+--ALTER TABLE 
+--ALTER TABLE branch ALTER COLUMN address TYPE varchar(400);
 CREATE TABLE IF NOT EXISTS employee(
     IFSC_code varchar(50) NOT NULL,
     employee_id serial PRIMARY KEY,
     name varchar(50) NOT NULL,
-    address varchar(50) NOT NULL,
-    pan_number varchar(50) UNIQUE NOT NULL,
-    adhare_number integer UNIQUE NOT NULL,
+    address varchar(400) NOT NULL,
+    pan_number varchar(50) UNIQUE NOT NULL CHECK (LENGTH(pan_number) = 10),
+    adhare_number varchar UNIQUE NOT NULL CHECK (LENGTH(adhare_number) = 12),
     salary integer NOT NULL,
     dob date NOT NULL,
     date_of_joining date NOT NULL,
     gender gender NOT NULL,
-
-    CONSTRAINT employee_of_branch
-    FOREIGN KEY(IFSC_code) 
-    REFERENCES branch(IFSC_code)
+    CONSTRAINT employee_of_branch FOREIGN KEY(IFSC_code) REFERENCES branch(IFSC_code)
 );
 
 CREATE TABLE IF NOT EXISTS account_type_details(
     account_type account_type PRIMARY KEY,
     interest_rate integer NOT NULL,
-    account_management_charges integer   
+    account_management_charges integer
 );
-
 
 -- Sequence is used in account number to create a 11 digit account number
 CREATE SEQUENCE IF NOT EXISTS account_sequence start 10000000000 increment 1;
-
 
 CREATE TABLE IF NOT EXISTS account(
     IFSC_code varchar(50) NOT NULL,
@@ -50,14 +44,8 @@ CREATE TABLE IF NOT EXISTS account(
     balance integer DEFAULT 0,
     opening_date date NOT NULL,
     account_freezed boolean NOT NULL DEFAULT false,
-
-    CONSTRAINT account_in_branch
-    FOREIGN KEY(IFSC_code) 
-    REFERENCES branch(IFSC_code),
-
-    CONSTRAINT account_type_relation
-    FOREIGN KEY(account_type) 
-    REFERENCES account_type_details(account_type)
+    CONSTRAINT account_in_branch FOREIGN KEY(IFSC_code) REFERENCES branch(IFSC_code),
+    CONSTRAINT account_type_relation FOREIGN KEY(account_type) REFERENCES account_type_details(account_type)
 );
 
 CREATE TABLE IF NOT EXISTS account_interest(
@@ -68,11 +56,7 @@ CREATE TABLE IF NOT EXISTS account_interest(
     is_deposited boolean DEFAULT false,
     transaction_id integer NOT NULL,
     calculated_date date NOT NULL,
-
-    CONSTRAINT account_interest_relation
-    FOREIGN KEY(account_number) 
-    REFERENCES account(account_number)
-
+    CONSTRAINT account_interest_relation FOREIGN KEY(account_number) REFERENCES account(account_number)
 );
 
 CREATE TABLE IF NOT EXISTS customer(
@@ -87,16 +71,9 @@ CREATE TABLE IF NOT EXISTS customer(
 CREATE TABLE IF NOT EXISTS acc_cust_relation(
     account_holder integer NOT NULL,
     account_number integer NOT NULL,
-
-    PRIMARY KEY(account_number,account_holder),
-
-    CONSTRAINT account_of_customer
-    FOREIGN KEY(account_holder) 
-    REFERENCES customer(adhare_number),
-
-    CONSTRAINT is_owner_of
-    FOREIGN KEY(account_number) 
-    REFERENCES account(account_number)
+    PRIMARY KEY(account_number, account_holder),
+    CONSTRAINT account_of_customer FOREIGN KEY(account_holder) REFERENCES customer(adhare_number),
+    CONSTRAINT is_owner_of FOREIGN KEY(account_number) REFERENCES account(account_number)
 );
 
 CREATE SEQUENCE IF NOT EXISTS transaction_sequence start 100000000000 increment 1;
@@ -109,14 +86,8 @@ CREATE TABLE IF NOT EXISTS transactions(
     transaction_type transaction_type NOT NULL,
     transaction_status boolean NOT NULL,
     transaction_time timestamptz DEFAULT NOW() NOT NULL,
-
-    CONSTRAINT sender_transaction_link
-    FOREIGN KEY(sender_account_number) 
-    REFERENCES account(account_number),
-
-    CONSTRAINT receiver_transaction_link
-    FOREIGN KEY(receiver_account_number) 
-    REFERENCES account(account_number)
+    CONSTRAINT sender_transaction_link FOREIGN KEY(sender_account_number) REFERENCES account(account_number),
+    CONSTRAINT receiver_transaction_link FOREIGN KEY(receiver_account_number) REFERENCES account(account_number)
 );
 
 CREATE TABLE IF NOT EXISTS loan_type_details(
@@ -128,21 +99,15 @@ CREATE TABLE IF NOT EXISTS loan_type_details(
 CREATE TABLE IF NOT EXISTS loan(
     account_number integer NOT NULL,
     loan_id integer NOT NULL PRIMARY KEY,
-    amount integer NOT NULL, 
+    amount integer NOT NULL,
     loan_type loan_type NOT NULL,
     emi integer NOT NULL,
     state_date date NOT NULL,
     transfer_transaction_id integer NOT NULL,
     duration_in_years integer NOT NULL,
     closed boolean NOT NULL,
-
-    CONSTRAINT loan_account_relation
-    FOREIGN KEY(account_number) 
-    REFERENCES account(account_number),
-
-    CONSTRAINT loan_type_relation
-    FOREIGN KEY(loan_type) 
-    REFERENCES loan_type_details(loan_type)
+    CONSTRAINT loan_account_relation FOREIGN KEY(account_number) REFERENCES account(account_number),
+    CONSTRAINT loan_type_relation FOREIGN KEY(loan_type) REFERENCES loan_type_details(loan_type)
 );
 
 CREATE TABLE IF NOT EXISTS loan_payments(
@@ -150,12 +115,8 @@ CREATE TABLE IF NOT EXISTS loan_payments(
     amount integer NOT NULL,
     loan_id integer NOT NULL,
     date_of_payment date NOT NULL,
-
-    CONSTRAINT loan_payment_relation
-    FOREIGN KEY(loan_id) 
-    REFERENCES loan(loan_id)
+    CONSTRAINT loan_payment_relation FOREIGN KEY(loan_id) REFERENCES loan(loan_id)
 );
-
 
 CREATE TABLE IF NOT EXISTS fixed_deposit(
     account_number integer NOT NULL,
@@ -164,17 +125,11 @@ CREATE TABLE IF NOT EXISTS fixed_deposit(
     interest_rate integer NOT NULL,
     period_in_years integer NOT NULL,
     closed boolean NOT NULL,
-
-    CONSTRAINT fd_account_relation
-    FOREIGN KEY(account_number) 
-    REFERENCES account(account_number)
+    CONSTRAINT fd_account_relation FOREIGN KEY(account_number) REFERENCES account(account_number)
 );
 
 CREATE TABLE IF NOT EXISTS fd_interest(
     id integer,
     interest_amount integer NOT NULL,
-
-    CONSTRAINT fd_interest_relation
-    FOREIGN KEY(id) 
-    REFERENCES fixed_deposit(id)
+    CONSTRAINT fd_interest_relation FOREIGN KEY(id) REFERENCES fixed_deposit(id)
 );
